d8961cd572556b2a42b62a770457ab2930642ed4
Jack-An <jackan2711@gmail.com>
2017-03-23 13:34:53 +0800

    Create Num.java

---
+++ NormalLearning/Num.java
@@ -0,0 +1,13 @@
+package com.jackTest;
+
+/**
+ * Created by Jack on 2017/3/15.
+ */
+public interface Num {
+    double PI=3.14159;
+    public int add(int a,int b);
+    public int subtract(int a,int b);
+    public int multiply(int a,int b);
+    public int divide(int a,int b);
+    public void printMessage();
+}
